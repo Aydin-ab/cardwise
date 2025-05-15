7a41d02470134ebc155389bc5de61c5cef8cca10
Aydin Abiar <aydinabiar@gmail.com>
2025-05-14 19:50:29 -0700

    style: added list of types to help doing good commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,16 @@
 module.exports = {extends: ['@commitlint/config-conventional']}
+/*
+[
+  'build',
+  'chore',
+  'ci',
+  'docs',
+  'feat',
+  'fix',
+  'perf',
+  'refactor',
+  'revert',
+  'style',
+  'test'
+];
+*/
