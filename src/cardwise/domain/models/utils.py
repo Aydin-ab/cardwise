b6b7f8955df901bb60534f9b1adf6575abe0a8fe
Aydin Abiar <aydinabiar@gmail.com>
2025-06-17 02:04:46 -0700

    refactor: deleted src/ layout

--- src/cardwise/domain/models/utils.py
+++
@@ -1,8 +0,0 @@
-def normalize_string(s: str) -> str:
-    """
-    Normalize a string by removing leading and trailing whitespace and converting to lowercase.
-    """
-    s_ = "".join([c.lower() for c in s if c.isalnum() or c == " "])
-    # remove spaces
-    s_ = s_.strip().replace(" ", "_")
-    return s_
